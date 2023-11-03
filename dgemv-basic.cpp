5bb5f1b0718934025e93681eb66d9a9c7cd5b927
Alfredo Ortiz <coderai@login34.chn>
2023-11-02 21:39:19 -0700

    update

--- dgemv-basic.cpp
+++ dgemv-basic.cpp
@@ -8,4 +8,16 @@ const char* dgemv_desc = "Basic implementation of matrix-vector multiply.";
  */
 void my_dgemv(int n, double* A, double* x, double* y) {
    // insert your code here: implementation of basic matrix multiply
+//	int offset = 0;
+
+	for(int row = 0; row < n; row++){
+		//offset = row * n; 
+
+		for(int col = 0; col < n; col++)
+		{
+			//y[row] += A[col+offset] * x[col];
+			y[row] += A[row * n + col] * x[col];
+		}
+	}
+
 }
